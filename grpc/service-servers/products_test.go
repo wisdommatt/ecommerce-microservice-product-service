d73d9e17c49dd989a34660695799dc6f9aad22c6
Wisdom Matthew <talk2wisdommatt@gmail.com>
2021-11-11 15:57:42 +0100

    refactor(tests): refactor `TestProductServer_GetProduct` test to use product service mock generated by mockery

--- grpc/service-servers/products_test.go
+++ grpc/service-servers/products_test.go
@@ -10,7 +10,6 @@ import (
 	"github.com/wisdommatt/ecommerce-microservice-product-service/grpc/proto"
 	"github.com/wisdommatt/ecommerce-microservice-product-service/internal/products"
 	"github.com/wisdommatt/ecommerce-microservice-product-service/mocks"
-	productmock "github.com/wisdommatt/ecommerce-microservice-product-service/test/mocks/products"
 	"google.golang.org/grpc/metadata"
 )
 
@@ -94,38 +93,35 @@ func TestProductServer_AddProduct(t *testing.T) {
 }
 
 func TestProductServer_GetProduct(t *testing.T) {
+	productService := &mocks.ProductService{}
+	productService.On("GetProduct", mock.Anything, "sku.invalid").Return(nil, errors.New("an error occured"))
+	productService.On("GetProduct", mock.Anything, "sku.valid").Return(&products.Product{
+		Sku: "sku.valid", Name: "HP 2223", Description: "Slim HP laptop",
+	}, nil)
+
 	type args struct {
 		input *proto.GetProductInput
 	}
 	tests := []struct {
-		name                  string
-		args                  args
-		serviceGetProductFunc func(ctx context.Context, sku string) (*products.Product, error)
-		want                  *proto.Product
-		wantErr               bool
+		name    string
+		args    args
+		want    *proto.Product
+		wantErr bool
 	}{
 		{
-			name: "GetProduct service implementation with error",
-			args: args{input: &proto.GetProductInput{Sku: "sku.eee"}},
-			serviceGetProductFunc: func(ctx context.Context, sku string) (*products.Product, error) {
-				return nil, errors.New("an error occured")
-			},
+			name:    "GetProduct service implementation with error",
+			args:    args{input: &proto.GetProductInput{Sku: "sku.invalid"}},
 			wantErr: true,
 		},
 		{
 			name: "GetProduct service implementation without error",
-			args: args{input: &proto.GetProductInput{Sku: "sku.eee"}},
-			serviceGetProductFunc: func(ctx context.Context, sku string) (*products.Product, error) {
-				return &products.Product{Name: "HP 2223", Description: "Slim HP laptop"}, nil
-			},
-			want: &proto.Product{Name: "HP 2223", Description: "Slim HP laptop"},
+			args: args{input: &proto.GetProductInput{Sku: "sku.valid"}},
+			want: &proto.Product{Sku: "sku.valid", Name: "HP 2223", Description: "Slim HP laptop"},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			s := NewProductServer(&productmock.ServiceMock{
-				GetProductFunc: tt.serviceGetProductFunc,
-			})
+			s := NewProductServer(productService)
 			got, err := s.GetProduct(context.TODO(), tt.args.input)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("ProductServer.GetProduct() error = %v, wantErr %v", err, tt.wantErr)
